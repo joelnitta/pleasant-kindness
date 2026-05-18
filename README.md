e6cac059a4096e89e0943674fc929cd3fb5aebb4
joelnitta <joelnitta@gmail.com>
2026-05-18 14:49:31 +0900

    Add basic file architecture

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pleasant Kindness - Django App
+
+A simple Django app with "Hello World" homepage.
+
+## Setup
+
+1. Clone this repo
+2. Create a virtual environment: python -m venv venv
+3. Activate it: source venv/bin/activate (or venv\Scripts\activate on Windows)
+4. Install dependencies: pip install -r requirements.txt
+5. Run locally: python manage.py runserver
+
+## Deploy to Railway
+
+1. Push this repo to GitHub
+2. Connect it to Railway
+3. Railway will automatically deploy!
+
+The app will be available at your Railway domain.
---
+++ myproject/settings.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-dev-key-change-in-production'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'myapp',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'myproject.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE', 'postgres'),
+        'USER': os.environ.get('PGUSER', 'postgres'),
+        'PASSWORD': os.environ.get('PGPASSWORD', ''),
+        'HOST': os.environ.get('PGHOST', 'localhost'),
+        'PORT': os.environ.get('PGPORT', '5432'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
